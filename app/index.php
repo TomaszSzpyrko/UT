d516ac87600dd4ece7ca6f35d9cfa342e4f3732c
Tomasz Szpyrko <92452586+TomaszSzpyrko@users.noreply.github.com>
2021-10-14 18:32:51 +0100

    error fixed

--- app/index.php
+++ app/index.php
@@ -1,7 +1,8 @@
 <?php
+$result = "";
 //include BoxesCalculator::class;
 include "BoxesCalculator.php";
-//$result ="";
+
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
     "http://www.w3.org/TR/html4/loose.dtd">

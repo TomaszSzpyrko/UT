ce1df4e807057e140fb95c9c87f49d0a527c1e55
zden3k <tomek.szpyrko@gmail.com>
2021-10-17 10:34:18 +0100

    done?

--- app/BoxesCalculator.php
+++ app/BoxesCalculator.php
@@ -18,21 +18,21 @@ class BoxesCalculator
             $shirtsLeft = $shirtsLeft % $boxSize;
             $boxesToSend[$boxSize] = $numberOfBoxesOfSize;
 
-            if ($shirtsLeft < $nextBoxSize and $shirtsLeft > $previousBoxSize*1.5) {
+            if ($shirtsLeft < $nextBoxSize and $shirtsLeft > $previousBoxSize*1.51) {
                 if (!isset($boxesToSend[$nextBoxSize])){$boxesToSend[$nextBoxSize]=0;}
                 $boxesToSend[$nextBoxSize] = $boxesToSend[$nextBoxSize] + 1;
-                //$shirtsLeft = 0;
+                $shirtsLeft = 0;
             }
 
             if ($shirtsLeft < $previousBoxSize and $shirtsLeft > $lastBoxSize) {
                 if (!isset($boxesToSend[$previousBoxSize])){$boxesToSend[$previousBoxSize]=0;}
                 $boxesToSend[$previousBoxSize] =$boxesToSend[$previousBoxSize] + 1;
-                //$shirtsLeft = 0;
+                $shirtsLeft = 0;
             }
             if ($shirtsLeft < $lastBoxSize and $shirtsLeft > 0) {
                 if (!isset($boxesToSend[$lastBoxSize])){$boxesToSend[$lastBoxSize]=0;}
                 $boxesToSend[$lastBoxSize] = $boxesToSend[$lastBoxSize] + 1;
-                //$shirtsLeft = 0;
+                $shirtsLeft = 0;
             }
 
 
@@ -43,4 +43,4 @@ class BoxesCalculator
 }
 
 //$g = new BoxesCalculator();
-//print_r($g->getNumberOfBoxesAndSizes(499, [250, 500, 1000, 2000, 5000]));
+//print_r($g->getNumberOfBoxesAndSizes(4444, [250, 500, 1000, 2000, 5000]));
